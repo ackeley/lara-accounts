b13f451f673dc7133a941a4bbd75f1a32bd256b6
Ackeley Lennon <ackeley@gmail.com>
2016-11-18 17:17:54 -0200

    Estilizando login e adicionando plugin de auth token JWT

--- resources/assets/js/components/Login.vue
+++ resources/assets/js/components/Login.vue
@@ -1,30 +1,33 @@
 <template>
-    <div class="container">
+    <div class="container">        
         <div class="row">
-            <div class="col m4 offset-m4">
-                <h4 class="center-align">LaraAccounts</h4>
-                <div class="row">
-                    <form>
-                        <div class="row">
-                            <div class="input-field col s12">
-                                <input type="email" id="email" v-model="user.email">
-                                <label for="email">E-mail:</label>
+            <div class="col m4 offset-m4 ">                    
+                    <h2 class="center-align">
+                        <img src="../../img/laracounts.png" class="responsive-img">
+                    </h2>               
+                    
+                    <div class="row">
+                        <form>
+                            <div class="row">
+                                <div class="input-field col s12">
+                                    <input type="email" id="email" v-model="user.email">
+                                    <label for="email">E-mail:</label>
+                                </div>
                             </div>
-                        </div>
-                        <div class="row">
-                            <div class="input-field col s12">
-                                <input type="password" id="pass" v-model="user.password">
-                                <label for="pass">Senha:</label>
+                            <div class="row">
+                                <div class="input-field col s12">
+                                    <input type="password" id="pass" v-model="user.password">
+                                    <label for="pass">Senha:</label>
+                                </div>
                             </div>
-                        </div>
-                        <div class="row">
-                            <div class="col m12">
-                                <p class="center-align">
-                                    <button class="btn btn-large btn-default" type="button" @click="login()">Entrar</button>
-                                </p>
+                            <div class="row">
+                                <div class="col m12">
+                                    <p class="center-align">
+                                        <button class="btn btn-large btn-default" type="button" @click="login()">Entrar</button>
+                                    </p>
+                                </div>
                             </div>
-                        </div>
-                    </form>
+                        </form>                    
                 </div>
             </div>
         </div>
